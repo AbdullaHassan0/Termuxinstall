5e808bf071fa4593f09110bfb87de88c7176971a
Abdullahack <82338568+Abdullahack@users.noreply.github.com>
2021-04-30 16:56:22 +0300

    Create termuxinstall.py

---
+++ termuxinstall.py
@@ -0,0 +1,51 @@
+import os,sys
+
+print("""\033[1;96m
+
+\033[1;92m ✓ PAKIJAKAN DADABAZN TKAYA DARMACHO
+
+\033[1;97m
+
+--------------------------------------------------""") 
+
+os.system('pkg update')
+
+os.system('pkg upgrade')
+
+os.system('pkg install git')
+
+os.system('pkg install wget')
+
+os.system('pkg install curl')
+
+os.system('pkg install openssl')
+
+os.system('pkg install python')
+
+os.system('pkg install python')
+
+os.system('pip2 install requests')
+
+os.system('pip2 install mechanize')
+
+os.system('pip2 install bs4')
+
+os.system('pip2 install uncompyle6')
+
+os.system('pip2 install time')
+
+os.system('pip2 install hashlib')
+
+os.system('pip2 install string')
+
+os.system('pip2 install urllib')
+
+os.system('pip2 install datetime')
+
+print("""\033[1;96m
+
+\033[1;92m ✓ TAWAW HAMU PAKIJAKAN DABAZEN
+
+\033[1;97m
+
+--------------------------------------------------""") 
